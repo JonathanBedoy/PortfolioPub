d5847125164306936e4d07a31a703c6f114e35dc
Jonathan Bedoy <jonathanbedoy1@gmail.com>
2023-01-17 14:20:05 -0800

    fixed links

--- portfolio/src/containers/Portfolio/Portfolio.js
+++ portfolio/src/containers/Portfolio/Portfolio.js
@@ -11,7 +11,7 @@ const Portfolio = (props) => {
         "E-commerce website for eyelashe extensions, branded as 'Nia Lashes By Denise'.",
       description:
         'This e-commerce website allows the user to add eyelash extensions to their cart and checkout using PayPal. Along with being able to take a profound look at the eyelash being sold. This single-page applictation was built using VueJs as the front end JavaScript framework and styled using Bootstrap components. The back end serves the web app using express hosted on an AWS Lightsail Instance. As well as using NeDB as a MongoDB-like database.',
-      websiteLink: 'https://nialashesbydenise.com',
+      websiteLink: 'https://nia.jonathanbedoy.com/',
       githubRepoLink: 'https://github.com/JonathanBedoy/EcommerceWebApp',
       img: '/assets/nialashesexample.jpg',
       imagAlt: 'Nia Lashes By Denise website Static Render',
@@ -29,8 +29,8 @@ const Portfolio = (props) => {
       teaser:
         'Invoice generator with the capability of keeping track of inventory, and company debts.',
       description:
-        "This web app allows the user to keep tabs on their inventory, as well as intuitively generate invoices using inputed company and inventory data. Making an invoice generates a PDF file which can then be printed out. A single-page application built using React Js, and mostly styled with Bootstrap-React. Using NeDB, the user's inputted data is stored and only served once login is verified. \nTry it out!\nUsername: guest\nPassword: password ",
-      websiteLink: 'https://inventory.jonathanbedoy.com',
+        "This web app allows the user to keep tabs on their inventory, as well as intuitively generate invoices using inputted company and inventory data. Making an invoice generates a PDF file which can then be printed out. A single-page application built using React Js, and mostly styled with Bootstrap-React. Using NeDB, the user's inputted data is stored and only served once login is verified. \nTry it out!\nUsername: guest\nPassword: password ",
+      websiteLink: 'https://invoice.jonathanbedoy.com/',
       githubRepoLink: 'https://github.com/JonathanBedoy/InvoiceGenerator',
       img: '/assets/InventorySystemExample.png',
       imagAlt: 'Invoice Generator website Static Render',
